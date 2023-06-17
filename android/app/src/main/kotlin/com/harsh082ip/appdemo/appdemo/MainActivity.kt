cae0b9f0fe322a0eabee688058ebef565edd4e69
harsh082ip <109286547+harsh082ip@users.noreply.github.com>
2023-06-17 23:13:09 +0530

    initial commit

---
+++ android/app/src/main/kotlin/com/harsh082ip/appdemo/appdemo/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.harsh082ip.appdemo.appdemo
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/home_screen.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  var value = '';
+  _changeValue() {
+    value = 'You are a good Person😊';
+    setState(() {});
+  }
+
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'A Messege for you',
+          style: TextStyle(color: Colors.white),
+        ),
+        backgroundColor: Colors.deepPurpleAccent,
+      ),
+      body: Container(
+        height: MediaQuery.of(context).size.height,
+        width: MediaQuery.of(context).size.width,
+        color: Colors.deepPurpleAccent,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              onPressed: () {
+                _changeValue();
+              },
+              child: const Text(
+                'Click Here',
+                style: TextStyle(fontSize: 25.0),
+              ),
+            ),
+            const SizedBox(
+              height: 20.0,
+            ),
+            Text(
+              value,
+              style: const TextStyle(fontSize: 28.0, color: Colors.white),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
